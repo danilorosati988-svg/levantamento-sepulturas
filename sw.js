cb03dfd3dd3e59568603f0ce55ecc81541b8c149
danilorosati988-svg <danilorosati988@gmail.com>
2026-08-13 14:37:14 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='lev-sep-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
